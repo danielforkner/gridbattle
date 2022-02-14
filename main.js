a75cce231ea73a62bf727a1b07306dc9c6f0985c
danielforkner <daniel.forkner@gmail.com>
2022-02-13 22:55:02 -0500

    you can select a cell and queue movement

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const startBtn = document.querySelector('.startBtn');
-const rowList = document.querySelectorAll('.row');
+const cellList = document.querySelectorAll('.row');
 const TICKSPEED = 1000;
+let queue = [];
 let unavailableCells = [];
+let activeCell = [];
 let gridSize = 20;
 let gameOn = false;
+let isSelected = false;
 let tick = 0;
-// grid is rowList[i].cells[j]
+// grid is made of cellList[i].cells[j]
+
+window.addEventListener('keydown', keyDown);
 
 startBtn.addEventListener('click', (e) => {
     if (gameOn) {return};
@@ -26,7 +31,7 @@ function spawn() {
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
             // add +1 to appropriate cells
-            cell = rowList[i].cells[j];
+            cell = cellList[i].cells[j];
             if (cell.classList.contains('spawner')) {
                 current = parseInt(cell.innerText) + 1;
                 cell.innerText = current;
@@ -45,8 +50,10 @@ function placeInitialCells() {
     // place player in a random cell
     let num1 = randomCell();
     let num2 = randomCell();
-    let player = rowList[num1].cells[num2];
+    let player = cellList[num1].cells[num2];
     player.classList.add('playerStart', 'spawner', 'player', 'active');
+    activeCell.push(num1);
+    activeCell.push(num2);
     unavailableCells.push([num1, num2]);
     placeCPU(num1, num2);
 };
@@ -58,7 +65,7 @@ function placeCPU(i, j) {
         num1 = randomCell();
         num2 = randomCell();
     } while (i === num1 && j === num2);
-    rowList[num1].cells[num2].classList.add('cpuStart', 'spawner', 'cpu');
+    cellList[num1].cells[num2].classList.add('cpuStart', 'spawner', 'cpu');
 };
 
 function randomCell() {
@@ -70,3 +77,113 @@ function incrementTicks() {
     document.querySelector('.ticks').innerText = tick;
 };
 
+// navigate the grid
+function keyDown(event) {
+    let active = document.querySelector('.active');
+    let i = activeCell[0];
+    let j = activeCell[1];
+    let nextCell, currentCell;
+    currentCell = cellList[i].cells[j] 
+    if (currentCell.classList.contains('selected')) {
+        isSelected = true;
+    }
+
+    // space bar
+    if (event.keyCode === 32) {
+        selectCell(i, j);
+        return;
+    }
+
+    if (event.key == 'w' || event.key == 'ArrowUp') {
+        if (activeCell[0] == 0) {
+            return;
+        } else { 
+            i--;
+            active.classList.remove('active');
+            activeCell[0] = i;
+            nextCell = cellList[i].cells[j];
+            nextCell.classList.add('active');
+            // add cells to the queue to be conquered
+            if (isSelected) {
+                // original cell
+                queue.unshift(cellList[i + 1].cells[j]);
+                // next cell
+                queue.unshift(nextCell);
+                nextCell.classList.add('queued');
+            }
+        }
+    }
+    if (event.key == 's' || event.key == 'ArrowDown') {
+        if (activeCell[0] == (gridSize - 1)) {
+            return;
+        } else { 
+            i++;
+            active.classList.remove('active');
+            activeCell[0] = i;
+            nextCell = cellList[i].cells[j];
+            nextCell.classList.add('active');
+            // add cells to the queue to be conquered
+            if (isSelected) {
+                // original cell
+                queue.unshift(cellList[i - 1].cells[j]);
+                // next cell
+                queue.unshift(nextCell);
+                nextCell.classList.add('queued');
+            }
+        }
+    }
+    if (event.key == 'a' || event.key == 'ArrowLeft') {
+        if (activeCell[1] == 0) {
+            return;
+        } else { 
+            j--;
+            active.classList.remove('active');
+            activeCell[1] = j;
+            nextCell = cellList[i].cells[j]
+            nextCell.classList.add('active');
+            // add cells to the queue to be conquered
+            if (isSelected) {
+                // original cell
+                queue.unshift(cellList[j + 1].cells[j]);
+                // next cell
+                queue.unshift(nextCell);
+                nextCell.classList.add('queued');
+            }
+        }
+    }
+    if (event.key == 'd' || event.key == 'ArrowRight') {
+        if (activeCell[1] == (gridSize - 1)) {
+            return;
+        } else { 
+            j++;
+            active.classList.remove('active');
+            activeCell[1] = j;
+            nextCell = cellList[i].cells[j]
+            nextCell.classList.add('active');
+            // add cells to the queue to be conquered
+            if (isSelected) {
+                // original cell
+                queue.unshift(cellList[j - 1].cells[j]);
+                // next cell
+                queue.unshift(nextCell);
+                nextCell.classList.add('queued');
+            }
+        }
+    }
+};
+
+function selectCell(i, j) {
+    let selected = cellList[i].cells[j];
+    // if there is a selected cell anywhere, remove it
+    if (document.querySelector('.selected')) {
+        document.querySelector('.selected').classList.remove('selected');
+        isSelected = false;
+    };
+    // if trying to select a player cell, select it
+    if (selected.classList.contains('player')) {
+        selected.classList.add('selected');
+        isSelected = true;
+    } else {
+        return;
+    }
+};
